0b408537037a7d08e470c6b6789e7090f85f3c28
Mitsrael Souza <mitsrael9@gmail.com>
2021-05-07 15:39:53 -0300

    Saving dark theme on LocalStorage

--- change-theme.js
+++ change-theme.js
@@ -1,5 +1,26 @@
 const changeTheme = document.getElementById('change-theme');
+let darkMode = localStorage.getItem('darkMode')
+
+const enableDarkMode = () => {
+  document.body.classList.add('theme-dark')
+  localStorage.setItem('darkMode', 'enabled')
+}
+
+const disableDarkMode = () => {
+  document.body.classList.remove('theme-dark')
+  localStorage.setItem('darkMode', null)
+}
+
+if (darkMode === 'enabled') {
+	enableDarkMode()
+}
 
 changeTheme.addEventListener('change', () => {
-	document.body.classList.toggle('theme-dark');
+	darkMode = localStorage.getItem('darkMode')
+
+	if (darkMode !== "enabled") {
+		enableDarkMode();
+	} else {
+		disableDarkMode();
+	}
 });
