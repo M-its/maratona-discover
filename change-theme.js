b427f7ba3f92c549ceef9ccadf93c77d900d368a
Mitsrael Souza <mitsrael9@gmail.com>
2021-05-08 10:11:14 -0300

    Fixed bug on checkbox

--- change-theme.js
+++ change-theme.js
@@ -1,13 +1,16 @@
 const changeTheme = document.getElementById('change-theme');
 let darkMode = localStorage.getItem('darkMode')
+const checkToggler = document.querySelector('#change-theme #checkbox')
 
 const enableDarkMode = () => {
   document.body.classList.add('theme-dark')
+  checkToggler.checked = true
   localStorage.setItem('darkMode', 'enabled')
 }
 
 const disableDarkMode = () => {
   document.body.classList.remove('theme-dark')
+  checkToggler.checked = false
   localStorage.setItem('darkMode', null)
 }
 
